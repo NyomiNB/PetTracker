f6785765173aafc8570f4825118ac820c41476da
Nyomi <nyominb@gmail.com>
2025-02-17 15:06:45 -0500

    Finished createUser() method in AuthViewModel file

--- PetTracker/PetTracker/AuthViewModel.swift
+++ PetTracker/PetTracker/AuthViewModel.swift
@@ -7,8 +7,10 @@
 
 import Foundation
 import FirebaseAuth
-
-
+import Firebase
+import FirebaseFirestore
+import FirebaseCore
+ 
 class AuthViewModel: ObservableObject{
     @Published var userSession: FirebaseAuth.User?
     @Published var currentUser: User?
@@ -22,7 +24,15 @@ class AuthViewModel: ObservableObject{
     
     func createUser(withEmail email: String, password: String, fullname: String) async throws{
         print("createAccount")
-
+        do{
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            let user = User(id: result.user.uid, fullName: fullname, email: email)
+            let encodedUser = try Firestore.Encoder().encode(user)
+            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+        } catch{
+            print("DEBUG: Failed to create user with error \(error.localizedDescription)")
+        }
     }
     
     func signOut(){
