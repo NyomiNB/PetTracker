da6da7689637b459acef7fd8d3c07ab5f3e7a11d
Nyomi <nyominb@gmail.com>
2025-03-31 13:48:25 -0400

    Users are now able to see notes and pet name in listview navigation list view

---
+++ PetTracker/PetTracker/Views/PetDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PetDetail.swift
+//  PetTracker
+//
+//  Created by Nyomi Bell on 3/26/25.
+//
+
+import SwiftUI
+
+struct PetDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PetDetail()
+}
